b91cf021eb1af80f6bae521bd25db36cac3f38c5
Tim Jockers <tim.jockers@icloud.com>
2026-08-25 20:31:18 +0200

    Implemented relayEvent and buttonEvent, fixed CMake

--- events.cpp
+++ events.cpp
@@ -15,3 +15,21 @@ bool irrigationEvent::isActive()
 {
     return false;
 }
+
+
+
+relayEvent::relayEvent(int r)
+    : relay(r)
+{}
+
+std::string relayEvent::getInfo()
+{
+    return "RelayEvent: R" + to_string(relay);
+}
+
+
+
+
+buttonEvent::buttonEvent(int r, std::chrono::seconds irrDuration)
+    : relayEvent(r), duration(irrDuration)
+{}
--- events.hpp
+++ events.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <chrono>
 
 class irrigationEvent {
 public:
@@ -9,4 +10,23 @@ public:
     virtual void activate();
     virtual void deactivate();
     virtual bool isActive();
+};
+
+class relayEvent : public irrigationEvent {
+public:
+    relayEvent(int r);
+
+    virtual std::string getInfo() override;
+
+protected:
+    int relay;
+};
+
+
+class buttonEvent : public relayEvent {
+public:
+    buttonEvent(int r, std::chrono::seconds irrDuration);
+
+protected:
+    std::chrono::seconds duration;
 };
