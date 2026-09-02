095446014d1f408a20f34eaa77428486e74da2a8
Tim Jockers <tim.jockers@icloud.com>
2026-09-02 15:17:14 +0200

    Added test for isButtonPressed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <chrono>
 #include <unistd.h>
 #include <iostream>
+#include <thread>
 
 int main() {
     /*buttonEvent *e = new buttonEvent(0, std::chrono::seconds(10));
@@ -23,7 +24,7 @@ int main() {
 
     ioManager *ioM = new ioManager(manager);
 
-    int relay;
+    /*int relay;
     int state;
     while (true)
     {
@@ -42,6 +43,15 @@ int main() {
             std::cout << ioM->getYLED(YLEDS[i]) << " ";
         }
         std::cout << std::endl;
+    }*/
+    while (true)
+    {
+        for (unsigned int i = 0; i < 8; i++)
+        {
+            std::cout << ioM->isButtonPressed(BUTTONS[i]) << " ";
+        }
+        std::cout << std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 
     return 0;
