18f7470a0ad0522062af57cf6a8c122427d42c1e
Tim Jockers <tim.jockers@icloud.com>
2026-09-03 20:07:46 +0200

    Cleaned main

--- main.cpp
+++ main.cpp
@@ -8,23 +8,6 @@
 #include <thread>
 
 int main() {
-    buttonEvent *e = new buttonEvent(Relay::R2, std::chrono::seconds(10));
-
-    std::cout << e->getInfo() << std::endl << std::endl;
-    
-    e->activate();
-
-    while (e->isActive())
-    {
-        std::this_thread::sleep_for(
-            std::chrono::seconds(1)
-        );
-        std::cout << "Running" << std::endl;
-    }
-    std::cout << "Done" << std::endl;
-    return 0;
-
-
     ConfigManager *manager = new ConfigManager("taqua.cfg");
 
     ioManager *ioM = new ioManager(manager);
