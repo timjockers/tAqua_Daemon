3d52e1b140c6ffe01a1af267762ca2fa41b6df31
Tim Jockers <tim.jockers@icloud.com>
2026-09-07 19:27:38 +0200

    Started implementation of ButtonController class

--- button_controller.cpp
+++ button_controller.cpp
@@ -1,1 +1,6 @@
 #include "button_controller.hpp"
+
+
+ButtonController::ButtonController(ConfigManager *configManager, QueueManager *queueManager)
+    : configM(configManager), queueM(queueManager)
+{}
--- button_controller.hpp
+++ button_controller.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "types.hpp"
+#include "fileexchange.hpp"
+#include "queue.hpp"
+
+
+class ButtonController {
+public:
+    explicit ButtonController(ConfigManager *configManager, QueueManager *queueManager);
+
+private:
+    ConfigManager *configM;
+    QueueManager *queueM;
+};
