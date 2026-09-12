f042fbcb4c2384f2fe072226912ad949c4f5a2a9
Tim Jockers <tim.jockers@icloud.com>
2026-09-12 11:56:58 +0200

    Improved setEventVector function

--- scheduler.cpp
+++ scheduler.cpp
@@ -6,7 +6,13 @@ Scheduler::Scheduler(QueueManager *queueManager)
 
 void Scheduler::setEventVector(const std::vector<scheduledEvent>& events)
 {
-    scheduledEvents = events;
+    scheduledEvents.clear();
+    scheduledEvents.reserve(events.size());
+
+    for (const auto& event : events)
+    {
+        scheduledEvents.push_back(event);
+    }
 }
 
 void Scheduler::work()
